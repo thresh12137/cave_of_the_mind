c34c25472156ddbe8e7d22b7c459ca00fff6b921
JSchiffb <159210124+Lumpcious@users.noreply.github.com>
2025-02-11 10:45:32 -0900

    Added Sounds
    Created trigger based sounds

---
+++ Assets/Scripts/TriggerSFX.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSFX : MonoBehaviour
+{
+    public AudioSource playsound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        playsound.Play();   
+    }
+}
