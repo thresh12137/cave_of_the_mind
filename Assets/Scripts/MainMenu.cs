b6348e76f33c5ee1f5a0ff5f8f1fbd11f753edac
Lulz23 <bluesnakeuser@gmail.com>
2025-03-03 19:20:42 -0700

    Created new scene for main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame() {
+
+        SceneManager.LoadSceneAsync(1);
+    }
+}
