e819131ceb4fe008d870a86684640990baf757fb
Lulz23 <bluesnakeuser@gmail.com>
2025-03-03 22:28:21 -0700

    Created basic main menu with option to quit game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
     public void PlayGame() {
 
         SceneManager.LoadSceneAsync(1);
+
+        //Hide the mouse cursor after loading the next scene
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void QuitGame() { 
+        Application.Quit();
     }
 }
